078586c622f03e461fac475f0c30dcb5166efd7e
Jessica Arozqueta <jarozque@ucsc.edu>
2017-07-06 17:18:32 -0700

    Add some info for Mummy

--- mars.py
+++ mars.py
@@ -1,2 +1,4 @@
 Cold and dry, and red, and have two rovers going
-The two mooons may be problem to something. 
+The two mooons may be problem to something.
+Buth the Mummy will appreciate the lack 
+Something here 
