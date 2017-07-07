a775e413f7ef4cc09954ef7ad20f298677e4dbd1
Jessica Arozqueta <jarozque@ucsc.edu>
2017-07-06 17:22:36 -0700

    Fix line that wasn

--- mars.py
+++ mars.py
@@ -1,4 +1,4 @@
 Cold and dry, and red, and have two rovers going
 The two mooons may be problem to something.
-Buth the Mummy will appreciate the lack 
-Something here 
+But the Mummy will appreciate the lack 
+We will need to manufacture out own oxygen 
